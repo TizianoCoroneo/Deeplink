0a5e80b4102a48012df8595dbcd21299e508c513
Tiziano Coroneo <tiziano@ticketswap.com>
2021-03-21 11:38:32 +0100

    chore: bumped swift tools version and specified platforms

--- Package.swift
+++ Package.swift
@@ -1,9 +1,13 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "Deeplink",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+    ],
     products: [
         .library(
             name: "Deeplink",
